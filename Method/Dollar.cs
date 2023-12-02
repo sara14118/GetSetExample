0e67153292afe57ebb473195ff5eaa794c434165
sara <sara.alzadjali14@outlook.com>
2023-12-02 14:13:43 +0400

    Add project files.

---
+++ Method/Dollar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    public class Dollar
+    {
+
+        public decimal Amount;
+
+
+        public decimal Amount2
+        {
+            get
+            {
+                return this.Amount;
+            }
+            set
+            {
+                this.Amount = value;
+            }
+        }
+
+        public Dollar(decimal amount)
+        {
+                if (amount <= 0)
+            {
+                this.Amount = 0;
+            }
+        }
+        
+    } 
+   
+    
+   
+}
---
+++ Method/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Dollar dollar = new Dollar(1.99m);
+            dollar.Amount = 1.99m; // set
+            Console.WriteLine(dollar.Amount);// get
+
+
+       
+
+
+            
+        }
+    }
+}
